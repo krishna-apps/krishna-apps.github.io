d4dd765bc4b12efc164ec383ecdd7bbcc084bb1c
Mayank Chaudhari <mayank.srmu@gmail.com>
2021-06-06 08:17:04 +0530

    build

---
+++ docs/precache-manifest.428e693d193cedcfab7ba95a27d1b6de.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ffcad225d570ffebbf020213d950333",
+    "url": "/assets/cs.jpg"
+  },
+  {
+    "revision": "e8b40977098a173aed49dd735e058d6e",
+    "url": "/assets/cs.svg"
+  },
+  {
+    "revision": "7b87f6bb58973b2ec3880a20b26f62f8",
+    "url": "/assets/cs1.jpg"
+  },
+  {
+    "revision": "674eb2258024723e9844dad58d971f60",
+    "url": "/assets/cs2.jpg"
+  },
+  {
+    "revision": "4772d6d225c568690267c8adfbcefe8e",
+    "url": "/assets/custom-cloud.gif"
+  },
+  {
+    "revision": "aabc468e4bf2f63fc18173d427c4e232",
+    "url": "/assets/cv1.gif"
+  },
+  {
+    "revision": "5e5387316e6362ec53ac304fbfe34541",
+    "url": "/assets/graphics.gif"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/assets/logo.png"
+  },
+  {
+    "revision": "6f84073c6448a84b0887ba910bdaa453",
+    "url": "/assets/logo/logo-black.png"
+  },
+  {
+    "revision": "70ec666e4b85a51a271b29871f1c6b34",
+    "url": "/assets/logo/logo-blue.png"
+  },
+  {
+    "revision": "967c2cecf99efaec331c5a0fff219940",
+    "url": "/assets/logo/logo-inverse.png"
+  },
+  {
+    "revision": "a1bb6101a187c9d9cd8cc4ceb91a86bc",
+    "url": "/assets/logo/logo-large.png"
+  },
+  {
+    "revision": "76407946cece5e4c98fa2275bc379049",
+    "url": "/assets/logo/logo.png"
+  },
+  {
+    "revision": "4e013350ff97317d9a0b3775d9bf18ae",
+    "url": "/assets/m.jpg"
+  },
+  {
+    "revision": "e26471674aacccb26af9392ccee6bf10",
+    "url": "/assets/m1.jpg"
+  },
+  {
+    "revision": "942d04683867a4ef346fb6a307956b50",
+    "url": "/assets/m1.png"
+  },
+  {
+    "revision": "92db88ad55cba1933d511dcbcdeb91cd",
+    "url": "/assets/mobile.gif"
+  },
+  {
+    "revision": "57d4ac3caf876fcf1a276b7cee83edb0",
+    "url": "/assets/vb.jpg"
+  },
+  {
+    "revision": "7944498a12c3fae0a91343523965fbe8",
+    "url": "/assets/vb1.jpg"
+  },
+  {
+    "revision": "96a55ed10ae739d9aab69b769c139ea3",
+    "url": "/assets/vb2.jpg"
+  },
+  {
+    "revision": "16238256cb028c608ab5ffdbfd8cb1cd",
+    "url": "/assets/wa.png"
+  },
+  {
+    "revision": "da87625292865a3ace48",
+    "url": "/css/app.1ac89c11.css"
+  },
+  {
+    "revision": "70ec666e4b85a51a271b29871f1c6b34",
+    "url": "/img/logo-blue.70ec666e.png"
+  },
+  {
+    "revision": "95a5e25d76912da216ef461a5e5cd1e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce5f83ff55f315650d14",
+    "url": "/js/about.567f4188.js"
+  },
+  {
+    "revision": "da87625292865a3ace48",
+    "url": "/js/app.a6024a9c.js"
+  },
+  {
+    "revision": "174dedc5184361f1bb07",
+    "url": "/js/chunk-vendors.72c31bf6.js"
+  },
+  {
+    "revision": "6a580ceaea88b7d8891d22d154bf329f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
